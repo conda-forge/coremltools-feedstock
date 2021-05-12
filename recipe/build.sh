79c7cbbaca3ffddfab4eb0d6b6c7e6088b25a412
Uwe L. Korn <uwe.korn@quantco.com>
2021-05-12 09:56:32 +0200

    Build protobuf if cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,30 @@
 
 set -e
 set -x
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
+  pushd deps/protobuf
+  (
+    export CC=$CC_FOR_BUILD
+    export CXX=$CXX_FOR_BUILD
+    unset CFLAGS
+    unset CXXFLAGS
+    export HOST=${BUILD}
+    export PREFIX=${BUILD_PREFIX}
+    aclocal
+    libtoolize
+    autoconf
+    autoreconf -i
+    automake --add-missing
+    ./configure --prefix="${PREFIX}" \
+            --build=${BUILD}         \
+            --host=${BUILD} || (cat config.log; exit 1)
+    make -j${CPU_COUNT}
+    make install
+  )
+  popd
+fi
+
 mkdir build
 cd build
 
