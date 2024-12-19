fc4d858bdc5b79e8ce75f7c0b0dcabfecec87f56
Mark Harfouche <mark.harfouche@gmail.com>
2024-12-19 12:58:41 -0500

    Try to remove the special casing for OSX arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-set -e
-set -x
-
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
-  pushd deps/protobuf
-  (
-    export CC=$CC_FOR_BUILD
-    export CXX=$CXX_FOR_BUILD
-    unset CFLAGS
-    unset CXXFLAGS
-    export HOST=${BUILD}
-    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-    aclocal
-    libtoolize
-    autoconf
-    autoreconf -i
-    automake --add-missing
-    ./configure --prefix="${BUILD_PREFIX}" \
-            --build=${BUILD}         \
-            --host=${BUILD} || (cat config.log; exit 1)
-    make -j${CPU_COUNT}
-    make install
-    cp src/js_embed ${BUILD_PREFIX}/bin/js_embed
-  )
-  popd
-fi
-
+set -ex
 mkdir build
 cd build
 
