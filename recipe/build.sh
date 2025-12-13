3fbdec4bc8158824b6588eb9391281981fbf6f48
Mark Harfouche <mark.harfouche@gmail.com>
2025-12-13 14:52:21 -0500

    Update dependencies and patches to unvendor protobuf

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,37 +3,41 @@
 set -e
 set -x
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" && "${CROSSCOMPILING_EMULATOR}" == "" ]]; then
-  pushd deps/protobuf
-  (
-    export CC=$CC_FOR_BUILD
-    export CXX=$CXX_FOR_BUILD
-    unset CFLAGS
-    unset CXXFLAGS
-    export HOST=${BUILD}
-    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-    aclocal
-    libtoolize
-    autoconf
-    autoreconf -i
-    automake --add-missing
-    ./configure --prefix="${BUILD_PREFIX}" \
-            --build=${BUILD}         \
-            --host=${BUILD} || (cat config.log; exit 1)
-    make -j${CPU_COUNT}
-    make install
-  )
-  popd
+export PREFIX=${PREFIX:-${CONDA_PREFIX}}
+
+# Make sure these dependencies don't get pulled in
+rm -rf deps/protobuf
+rm -rf deps/kmeans1d
+
+if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == "1" ]]; then
+    Protobuf_PROTOC_EXECUTABLE=${BUILD_PREFIX}/bin/protoc
+else
+    Protobuf_PROTOC_EXECUTABLE=${PREFIX}/bin/protoc
 fi
 
-mkdir build
+# Their CMake script isn't the best, they expect these
+# protobuf files to have been pregenerated
+# So we remove them, and explicitely rebuild them below
+# Remove the pregenerated protobuf files
+rm -rf mlmodel/build
+# Make the folder structure they expect
+mkdir -p mlmodel/build/format
+
+mkdir -p build
 cd build
 
 cmake ${CMAKE_ARGS} \
+    -DOVERWRITE_PB_SOURCE=ON \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DProtobuf_PROTOC_EXECUTABLE=${Protobuf_PROTOC_EXECUTABLE} \
     -DPython_FIND_STRATEGY:STRING=LOCATION \
     -DPython_ROOT_DIR:FILEPATH="${PREFIX}" \
     ..
+
+# Build the protobuf source files first
+cmake --build . --target protosrc
+
+# Build the rest of the project
 make -j ${CPU_COUNT}
 
-${PYTHON} -m pip install --no-deps --ignore-installed ../
+${PYTHON:-python} -m pip install --no-deps --ignore-installed ../
